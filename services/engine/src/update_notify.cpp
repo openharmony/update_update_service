4e6f884ee0ca5265292605cf65a9b7e46916fb3c
邹友松 <zouyousong1@h-partners.com>
2023-09-25 09:18:14 +0800

    修改hap通知消息空白 Signed-off-by: 邹友松 <zouyousong1@h-partners.com>

--- services/engine/src/update_notify.cpp
+++ services/engine/src/update_notify.cpp
@@ -15,88 +15,38 @@
 
 #include "update_notify.h"
 
-#ifdef ABILITY_RUNTIME_ENABLE
-#include "ability_manager_proxy.h"
-#endif
 #include "iservice_registry.h"
 
 #include "update_helper.h"
-#include "update_log.h"
-
-using namespace OHOS;
-using namespace OHOS::AAFwk;
-using namespace OHOS::AppExecFwk;
-using namespace std;
+#include "update_log.h"`1
 
 namespace OHOS {
 namespace UpdateEngine {
-static sptr<AAFwk::IAbilityManager> g_abilityMgr;
-
-bool UpdateNotify::InitAbilityManagerService()
+ErrCode UpdateNotify::StartAbility(const AAFwk::Want &want)
 {
-    if (g_abilityMgr != nullptr) {
-        return true;
-    }
-    sptr<ISystemAbilityManager> systemAbilityManager =
-        SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
-    if (systemAbilityManager == nullptr) {
-        ENGINE_LOGE("Failed to get system ability mgr.");
-        return false;
-    }
-    sptr<IRemoteObject> remoteObject = systemAbilityManager->GetSystemAbility(ABILITY_MGR_SERVICE_ID);
-    if (remoteObject == nullptr) {
-        ENGINE_LOGE("Failed to get ability manager service.");
-        return false;
-    }
-    g_abilityMgr = iface_cast<IAbilityManager>(remoteObject);
-    if ((g_abilityMgr == nullptr) || (g_abilityMgr->AsObject() == nullptr)) {
-        ENGINE_LOGE("Failed to get system ability manager services ability");
-        return false;
-    }
-    return true;
-}
-
-ErrCode UpdateNotify::StartAbility(const Want &want)
-{
-    if (!InitAbilityManagerService()) {
-        ENGINE_LOGE("failed to get ability manager service");
-        return OHOS::ERR_INVALID_VALUE;
-    }
-    ErrCode result = g_abilityMgr->StartAbility(want);
+    ErrCode result = AAFwk::AbilityManagerClient::GetInstance()->StartAbility(want);
     ENGINE_LOGI("StartAbility result %{public}d", result);
     return result;
 }
 
-ErrCode UpdateNotify::StopServiceAbility(const Want &want)
+ErrCode UpdateNotify::StopServiceAbility(const AAFwk::Want &want)
 {
-    if (!InitAbilityManagerService()) {
-        ENGINE_LOGE("failed to get ability manager service");
-        return OHOS::ERR_INVALID_VALUE;
-    }
-    ErrCode result = g_abilityMgr->StopServiceAbility(want);
+    ErrCode result = AAFwk::AbilityManagerClient::GetInstance()->StopServiceAbility(want);
     ENGINE_LOGI("StopServiceAbility result %{public}d", result);
     return result;
 }
 
-ErrCode UpdateNotify::ConnectAbility(const Want &want, const sptr<IAbilityConnection> &connect,
+ErrCode UpdateNotify::ConnectAbility(const AAFwk::Want &want, const sptr<AAFwk::IAbilityConnection> &connect,
     const sptr<IRemoteObject> &callerToken)
 {
-    if (!InitAbilityManagerService()) {
-        ENGINE_LOGE("failed to get ability manager service");
-        return OHOS::ERR_INVALID_VALUE;
-    }
-    ErrCode result = g_abilityMgr->ConnectAbility(want, connect, callerToken);
+    ErrCode result = AAFwk::AbilityManagerClient::GetInstance()->ConnectAbility(want, connect, callerToken);
     ENGINE_LOGI("ConnectAbility result %{public}d", result);
     return result;
 }
 
-ErrCode UpdateNotify::DisconnectAbility(const sptr<IAbilityConnection> &connect)
+ErrCode UpdateNotify::DisconnectAbility(const sptr<AAFwk::IAbilityConnection> &connect)
 {
-    if (!InitAbilityManagerService()) {
-        ENGINE_LOGE("failed to get ability manager service");
-        return OHOS::ERR_INVALID_VALUE;
-    }
-    ErrCode result = g_abilityMgr->DisconnectAbility(connect);
+    ErrCode result = AAFwk::AbilityManagerClient::GetInstance()->DisconnectAbility(connect);
     ENGINE_LOGI("DisconnectAbility result %{public}d", result);
     return result;
 }
@@ -104,8 +54,8 @@ ErrCode UpdateNotify::DisconnectAbility(const sptr<IAbilityConnection> &connect)
 Want UpdateNotify::MakeWant(const std::string &deviceId, const std::string &abilityName, const std::string &bundleName,
     const std::string &subscribeInfo, const std::string &params)
 {
-    ElementName element(deviceId, bundleName, abilityName);
-    Want want;
+    AppExecFwk::ElementName element(deviceId, bundleName, abilityName);
+    AAFwk::Want want;
     want.SetElement(element);
     want.SetParam("EventInfo", params);
     want.SetParam("SubscribeInfo", subscribeInfo);
@@ -120,11 +70,8 @@ void UpdateNotify::NotifyToAppService(const std::string &eventInfo, const std::s
     }
     string bundleName = OUC_PACKAGE_NAME;
     string abilityName = OUC_SERVICE_EXT_ABILITY_NAME;
-    Want want = MakeWant("", abilityName, bundleName, subscribeInfo, eventInfo);
-    ErrCode result = StartAbility(want);
-    if (result != OHOS::ERR_OK) {
-        ENGINE_LOGE("NotifyToAppService fail.");
-    }
+    AAFwk::Want want = MakeWant("", abilityName, bundleName, subscribeInfo, eventInfo);
+    return StartAbility(want) == OHOS::ERR_OK;
 }
 
 void NotifyConnection::OnAbilityConnectDone(const ElementName &element,
@@ -133,7 +80,7 @@ void NotifyConnection::OnAbilityConnectDone(const ElementName &element,
     ENGINE_LOGI("OnAbilityConnectDone successfully. result %{public}d", resultCode);
 }
 
-void NotifyConnection::OnAbilityDisconnectDone(const ElementName &element, int resultCode)
+void NotifyConnection::OnAbilityDisconnectDone(const AppExecFwk::ElementName &element, int resultCode)
 {
     ENGINE_LOGI("OnAbilityDisconnectDone successfully. result %{public}d", resultCode);
 }
