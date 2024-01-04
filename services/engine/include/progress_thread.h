b2d1430cfcea6c81d06b98786fe3af5e5733eb91
flyinghors <zhanghaiyang43@huawei.com>
2024-01-04 10:24:35 +0000

    Description:版本告警问题处理 Team:OTHERS Feature or Bugfix:Bugfix Binary Source:No PrivateCode(Yes/No):No
    Signed-off-by: flyinghors <zhanghaiyang43@huawei.com>

--- services/engine/include/progress_thread.h
+++ services/engine/include/progress_thread.h
@@ -90,6 +90,7 @@ protected:
     bool ProcessThreadExecute() override;
     void ProcessThreadExit() override;
     int32_t DownloadCallback(uint32_t percent, UpgradeStatus status, const std::string &error);
+    static FILE* FileOpen(const std::string &fileName, const std::string &mode);
 
 private:
     Progress downloadProgress_ {};
