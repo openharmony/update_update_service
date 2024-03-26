1c682cd9da11c7647b2aaedb411b5388a25cf3ee
黄四平 <huangsiping3@huawei.com>
2024-03-26 09:54:54 +0000

    update services/firmware/common/src/firmware_update_adapter.cpp.
    Signed-off-by: 黄四平 <huangsiping3@huawei.com>

--- services/firmware/common/src/firmware_update_adapter.cpp
+++ services/firmware/common/src/firmware_update_adapter.cpp
@@ -37,37 +37,6 @@
 
 namespace OHOS {
 namespace UpdateEngine {
-constexpr int32_t REQUEST_ID_LEN = 21;
-constexpr int32_t TIME_STAMP_LEN = 15;
-constexpr int32_t TIME_START_YEAR = 1900;
-constexpr int32_t RANDOM_BOUND_SIX = 1000000;
-
-std::string FirmwareUpdateAdapter::GetRequestId()
-{
-    char requestId[REQUEST_ID_LEN] = {0};
-    char timeStamp[TIME_STAMP_LEN] = {0};
-    struct tm timeInfo;
-    time_t now;
-    time(&now);
-    localtime_r(&now, &timeInfo);
-    int res = sprintf_s(timeStamp, sizeof(timeStamp), "%d%02d%02d%02d%02d%02d", timeInfo.tm_year + TIME_START_YEAR,
-        timeInfo.tm_mon + 1, timeInfo.tm_mday, timeInfo.tm_hour, timeInfo.tm_min, timeInfo.tm_sec);
-    if (res <= 0) {
-        FIRMWARE_LOGE("CreateTimeStamp error: %{public}d", res);
-        return "";
-    }
-
-    srand((unsigned int)now);
-    int32_t randomInt = rand() % RANDOM_BOUND_SIX; /* random len is 6 */
-    res = sprintf_s(requestId, REQUEST_ID_LEN, "%s%d", timeStamp, randomInt);
-    if (res <= 0) {
-        FIRMWARE_LOGE("CreateRequestId error: %{public}d", res);
-        return "";
-    }
-
-    return std::string(requestId);
-}
-
 std::string FirmwareUpdateAdapter::GetBusinessDomain()
 {
     return DelayedSingleton<ConfigParse>::GetInstance()->GetBusinessDomain();
