a43a204e4e050817655ad3e7102451213ef86c38
RainyDay <wangning.wang@huawei.com>
2025-06-18 14:12:11 +0800

    Signed-off-by: RainyDay <wangning.wang@huawei.com>

---
+++ frameworks/js/ani/include/ani_base_updater.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANI_BASE_UPDATER_H
+#define ANI_BASE_UPDATER_H
+
+#include <mutex>
+#include <vector>
+
+#include "ohos.update.EventInfo.proj.1.hpp"
+#include "taihe/callback.hpp"
+
+#include "business_error.h"
+#include "event_info.h"
+
+namespace OHOS::UpdateEngine {
+class AniBaseUpdater {
+public:
+    virtual ~AniBaseUpdater();
+    AniBaseUpdater() = default;
+
+    void On(taihe::callback_view<void(const ohos::update::EventInfo &)> callback);
+    void Off(const taihe::callback<void(const ohos::update::EventInfo &)> &callback);
+    void OffAll();
+
+protected:
+    virtual void RegisterCallback() {}
+    virtual std::string GetPermissionName();
+    virtual void UnRegisterCallback() {}
+    bool SetError(int32_t ret, const std::string &funcName, const BusinessError &error);
+    void CallbackEventInfo(const EventInfo &eventInfo);
+
+private:
+    BusinessError GetIpcBusinessError(const std::string &funcName, int32_t ipcRequestCode);
+    bool IsCommonError(CallResult callResult);
+    int32_t ConvertToErrorCode(CallResult callResult);
+
+    std::mutex mutex_;
+    std::vector<taihe::callback<void(const ohos::update::EventInfo &)>> callbacks_;
+};
+}
+
+#endif // ANI_BASE_UPDATER_H
---
+++ frameworks/js/ani/include/ani_local_updater.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANI_LOCAL_UPDATER_H
+#define ANI_LOCAL_UPDATER_H
+
+#include "ohos.update.UpgradeFile.proj.1.hpp"
+
+#include "ani_base_updater.h"
+
+namespace OHOS::UpdateEngine {
+class AniLocalUpdater final : public AniBaseUpdater {
+public:
+    AniLocalUpdater() = default;
+
+    void VerifyUpgradePackageSync(const ohos::update::UpgradeFile &upgradeFile, taihe::string_view certsFile);
+    void ApplyNewVersionSync(const taihe::array_view<ohos::update::UpgradeFile> &upgradeFiles);
+};
+}
+#endif // ANI_LOCAL_UPDATER_H
