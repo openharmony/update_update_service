8a4298cf86e436c01358ed3ca874f6ae6abf9f8a
zouyousong123 <zouyousong1@h-partners.com>
2025-11-26 16:54:40 +0800

    修改检视意见
    Signed-off-by: zouyousong123 <zouyousong1@h-partners.com>

--- services/engine/include/update_service_restorer.h
+++ services/engine/include/update_service_restorer.h
@@ -18,7 +18,10 @@
 
 #include "iservice_restorer.h"
 
+#include "iservice_registry.h"
 #include "nocopyable.h"
+#include "storage_manager_proxy.h"
+#include "system_ability_definition.h"
 
 namespace OHOS {
 namespace UpdateService {
@@ -32,6 +35,9 @@ public:
 
     int32_t FactoryReset(BusinessError &businessError) override;
     int32_t ForceFactoryReset(BusinessError &businessError) override;
+private:
+    int32_t FileManagerEraseKeys();
+    static sptr<StorageManager::IStorageManager> GetStorageMgrProxy();
 };
 } // namespace UpdateService
 } // namespace OHOS
