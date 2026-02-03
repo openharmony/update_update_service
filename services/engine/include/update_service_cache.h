12ac81836d5d3a9176050f005bef7228d00c364a
zouyousong123 <zouyousong1@h-partners.com>
2026-02-03 17:01:06 +0800

    线程加锁
    Signed-off-by: zouyousong123 <zouyousong1@h-partners.com>

--- services/engine/include/update_service_cache.h
+++ services/engine/include/update_service_cache.h
@@ -49,7 +49,7 @@ private:
     static uint64_t paramCheckInterval_;
     static uint64_t paramDownloadInterval_;
     static UpgradeInterval paramUpgradeInterval_;
-    std::mutex upgradeInfoLock_;
+    std::mutex threadLock_;
 };
 } // namespace UpdateService
 } // namespace OHOS
