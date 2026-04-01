45ad8ff6fe8aa94767c83aed5ad1ebbd2ccb5eb4
zouyousong123 <zouyousong1@h-partners.com>
2026-04-01 11:03:46 +0800

    新增scope
    Signed-off-by: zouyousong123 <zouyousong1@h-partners.com>

--- frameworks/js/napi/update/src/update_session.cpp
+++ frameworks/js/napi/update/src/update_session.cpp
@@ -28,11 +28,9 @@ void UpdateAsyncession::CompleteWork(napi_env env, napi_status status)
 {
     ENGINE_LOGI("UpdateAsyncession::CompleteWork callbackNumber_: %{public}d, %{public}d",
         static_cast<int32_t>(callbackNumber_), sessionParams_.type);
-    napi_handle_scope scope = nullptr;
-    napi_open_handle_scope(env, &scope);
-    if (scope == nullptr) {
-        return;
-    }
+    napi_handle_scope scope;
+    napi_status status = napi_open_handle_scope(env, &scope);
+    PARAM_CHECK_NAPI_CALL(env, status == napi_ok, return, "Error open_handle_scope");
     UpdateResult result;
     GetUpdateResult(result);
     NotifyJS(env, NULL, result);
@@ -46,11 +44,9 @@ std::string BaseUpdateSession::GetFunctionName()
 
 void UpdatePromiseSession::CompleteWork(napi_env env, napi_status status)
 {
-    napi_handle_scope scope = nullptr;
-    napi_open_handle_scope(env, &scope);
-    if (scope == nullptr) {
-        return;
-    }
+    napi_handle_scope scope;
+    napi_status status = napi_open_handle_scope(env, &scope);
+    PARAM_CHECK_NAPI_CALL(env, status == napi_ok, return, "Error open_handle_scope");
     UpdateResult result;
     GetUpdateResult(result);
     NotifyJS(env, NULL, result);
