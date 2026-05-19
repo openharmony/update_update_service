34969eceb1146cef1207fc7a8aba8789516586e2
zouyousong123 <zouyousong1@h-partners.com>
2026-05-19 22:52:43 +0800

    新增不可见字符检查
    Signed-off-by: zouyousong123 <zouyousong1@h-partners.com>

--- services/firmware/utils/src/firmware_check_analyze_utils.cpp
+++ services/firmware/utils/src/firmware_check_analyze_utils.cpp
@@ -172,7 +172,7 @@ bool FirmwareCheckAnalyzeUtils::IsValidFileName(const std::string &fileName)
         }
     }
 
-    static constexpr std::string_view specialChars = "<>:\"|?* ";
+    const std::string specialChars = "<>:\"|?* \t\n\r";
     const auto invalidCharsPos = fileName.find_first_of(specialChars);
     if (invalidCharsPos != std::string::npos) {
         FIRMWARE_LOGE("FileName contains invalid chars %{public}s", fileName.c_str());
