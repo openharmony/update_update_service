b03f32d494709ab7a6337247d4c4385cc167a657
zouyousong123 <zouyousong1@h-partners.com>
2026-06-03 00:39:31 +0800

    修改判断
    Signed-off-by: zouyousong123 <zouyousong1@h-partners.com>

--- services/engine/src/update_service.cpp
+++ services/engine/src/update_service.cpp
@@ -670,7 +670,10 @@ bool UpdateService::IsPermissionGranted(uint32_t code)
 bool UpdaterService::IsMdmDisableReset(uint32_t code)
 {
     if (OHOS::UpdaterService::RESET_CODES.find(code) != OHOS::UpdaterService::RESET_CODES.end()) {
-        return OHOS::system::GetParameter(MDM_DISABLE_RESET_PARA.data(), DEFAULT_MDM_DISABLE_RESET_PARA.data());
+        std::string mdmResetParamData = OHOS::system::GetParameter(MDM_DISABLE_RESET_PARA.data(), DEFAULT_MDM_DISABLE_RESET_PARA.data());
+        std::transform(mdmResetParamData.begin(), mdmResetParamData.end(), mdmResetParamData.begin(),
+            [](unsigned char ch) { return static_cast<char>(tolower(ch)); });
+        return mdmResetParamData == "true";
     }
     return false;
 }
